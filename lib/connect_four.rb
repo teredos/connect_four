fdc2d18f614f529199f11863e060a282e19315f9
teredos <61703746+teredos@users.noreply.github.com>
2023-06-21 15:24:04 +0100

    Update connect_four.rb and its spec file

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,2 +1,127 @@
-# frozen_string_literal
+# frozen_string_literal: true
 
+require_relative 'grid'
+require_relative 'player'
+
+class ConnectFour
+  def initialize
+    @grid = Grid.new(6, 7)
+    @player1 = Player.new
+    @player2 = Player.new
+  end
+
+  def start
+    assign_names
+    assign_circles
+    @grid.show
+    start_round(@player1)
+  end
+
+  def assign_names
+    @player1.create_name(1)
+    @player2.create_name(2)
+  end
+
+  def assign_circles
+    puts "#{@player1.name}, would you like to play as the black circle or the white circle? [B/W]"
+    choice = gets.chomp
+    until %w[B W].include?(choice.upcase)
+      puts "Error: Only input 'B' or 'W' (case insensitive)"
+      choice = gets.chomp
+    end
+    @player1.assign_circle(choice.upcase == 'B' ? '○' : '●')
+    @player2.assign_circle(choice.upcase == 'B' ? '●' : '○')
+  end
+
+  def start_round(player)
+    puts "#{player.name}, which column would you like to place your #{player.circle} [1-7]?"
+    @grid.update(gets.chomp, player.circle)
+    if winner?(player.circle)
+      puts "#{player.name} wins!"
+    elsif @grid.full?
+      puts "It's a tie!"
+    else
+      start_round(player == @player1 ? @player2 : @player1)
+    end
+  end
+
+  def winner?(circle)
+    horizontal_winner?(circle) || vertical_winner?(circle) || diagonal_winner?(circle)
+  end
+
+  def horizontal_winner?(circle)
+    @grid.positions.any? { |row| row.join.include?(circle * 4) }
+  end
+
+  def vertical_winner?(circle)
+    7.times do |i|
+      column = []
+      @grid.positions.each { |row| column << row[i] }
+      return true if column.join.include?(circle * 4)
+    end
+    false
+  end
+
+  def diagonal_winner?(circle)
+    k = 2
+    3.times do
+      i = k
+      j = 0
+      diagonal = []
+      until i > 5
+        diagonal << @grid.positions[i][j]
+        i += 1
+        j += 1
+      end
+      return true if diagonal.join.include?(circle * 4)
+
+      k -= 1
+    end
+
+    k = 1
+    3.times do
+      i = 0
+      j = k
+      diagonal = []
+      until j > 6
+        diagonal << @grid.positions[i][j]
+        i += 1
+        j += 1
+      end
+      return true if diagonal.join.include?(circle * 4)
+
+      k += 1
+    end
+
+    k = 3
+    3.times do
+      i = k
+      j = 0
+      diagonal = []
+      until i.negative?
+        diagonal << @grid.positions[i][j]
+        i -= 1
+        j += 1
+      end
+      return true if diagonal.join.include?(circle * 4)
+
+      k += 1
+    end
+
+    k = 1
+    3.times do
+      i = 5
+      j = k
+      diagonal = []
+      until j > 6
+        diagonal << @grid.positions[i][j]
+        i -= 1
+        j += 1
+      end
+      return true if diagonal.join.include?(circle * 4)
+
+      k += 1
+    end
+    false
+  end
+end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -2,5 +2,103 @@
 
 require_relative '../lib/connect_four'
 
-describe '' do
+describe ConnectFour do
+  let(:connect_four) { described_class.new }
+
+  describe '#assign_circles' do
+    before(:each) do
+      allow(connect_four).to receive(:puts)
+    end
+    context 'when player input is valid' do
+      before(:each) do
+        allow(connect_four).to receive_message_chain(:gets,
+                                                     :chomp).and_return('B')
+      end
+      it "doesn't output an error" do
+        error = "Error: Only input 'B' or 'W' (case insensitive)"
+        expect(connect_four).not_to receive(:puts).with(error)
+        connect_four.assign_circles
+      end
+    end
+
+    context 'when player input is invalid once, then valid' do
+      before(:each) do
+        allow(connect_four).to receive_message_chain(:gets, :chomp).and_return(
+          '5', 'B'
+        )
+      end
+      it 'outputs an error once' do
+        error = "Error: Only input 'B' or 'W' (case insensitive)"
+        expect(connect_four).to receive(:puts).with(error).once
+        connect_four.assign_circles
+      end
+    end
+
+    context 'when player input is invalid thrice, then valid' do
+      before(:each) do
+        allow(connect_four).to receive_message_chain(:gets, :chomp).and_return(
+          'one', '2', 'three', 'W'
+        )
+      end
+      it 'outputs an error thrice' do
+        error = "Error: Only input 'B' or 'W' (case insensitive)"
+        expect(connect_four).to receive(:puts).with(error).thrice
+        connect_four.assign_circles
+      end
+    end
+  end
+
+  describe '#horizontal_winner?' do
+    context "when 4 instances of player's circle are positioned horizontally in grid" do
+      let(:horiz_grid) do
+        double('Grid', positions: Array.new(6) do
+                                    Array.new(7, '-')
+                                  end.reverse)
+      end
+      it 'returns true' do
+        grid = connect_four.instance_variable_set(:@grid, horiz_grid)
+        player_circle = '○'
+        four_circles = ['○', '○', '○', '○']
+        grid.positions[0][0..3] = four_circles
+        expect(connect_four.horizontal_winner?(player_circle)).to eq(true)
+      end
+    end
+  end
+
+  describe '#vertical_winner?' do
+    context "when 3 instances of player's circle are positioned vertically in grid" do
+      let(:vert_grid) do
+        double('Grid', positions: Array.new(6) do
+                                    Array.new(7, '-')
+                                  end.reverse)
+      end
+      it 'returns false' do
+        grid = connect_four.instance_variable_set(:@grid, vert_grid)
+        player_circle = '○'
+        grid.positions[0][0] = '○'
+        grid.positions[1][0] = '○'
+        grid.positions[2][0] = '○'
+        expect(connect_four.vertical_winner?(player_circle)).to eq(false)
+      end
+    end
+  end
+
+  describe '#diagonal_winner?' do
+    context "when 4 instances of player's circle are positioned diagonally in grid" do
+      let(:diag_grid) do
+        double('Grid', positions: Array.new(6) do
+                                    Array.new(7, '-')
+                                  end.reverse)
+      end
+      it 'returns true' do
+        grid = connect_four.instance_variable_set(:@grid, diag_grid)
+        player_circle = '○'
+        diag_grid.positions[0][0] = '○'
+        diag_grid.positions[1][1] = '○'
+        diag_grid.positions[2][2] = '○'
+        diag_grid.positions[3][3] = '○'
+        expect(connect_four.diagonal_winner?(player_circle)).to eq(true)
+      end
+    end
+  end
 end
